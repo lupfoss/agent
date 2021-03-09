d6e1cf7a9108bbc8ac6bc33b1c8cad11cbb16170
Manu Bansal <manub@lightup.ai>
2021-03-08 23:21:09 -0800

    bugfix

--- setup.sh
+++ setup.sh
@@ -2,7 +2,7 @@
 
 CONFIG_FILE=config.py
 
-pip3 install virtualenv
+[[ $(which virtualenv) ]] || pip3 install virtualenv
 cp config_template.py $CONFIG_FILE
 
 echo; echo "****** TODO: please set up values in $CONFIG_FILE *******"; echo
